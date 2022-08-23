25ecdcabced4a43b0dc77c4edd925edb3357a5f7
Perison1 <VP83097@globalymc.com>
2022-08-23 15:00:57 +0800

    prisoner

---
+++ ggkt_parent/service/service_vod/src/main/java/com/atguigu/vod/service/TeacherService.java
@@ -0,0 +1,17 @@
+package com.atguigu.vod.service;
+
+
+import com.atguigu.ggkt.model.vod.Teacher;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 讲师 服务类
+ * </p>
+ *
+ * @author prisoner
+ * @since 2022-08-23
+ */
+public interface TeacherService extends IService<Teacher> {
+
+}
